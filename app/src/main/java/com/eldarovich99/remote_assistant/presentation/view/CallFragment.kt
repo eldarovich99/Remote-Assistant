958d39d03d241380bf31fb4f84efa616ecdde562
eldarovich99 <32201422+eldarovich99@users.noreply.github.com>
2020-02-11 16:09:30 +0300

    added navigation on CallFragment for glasses

--- app/src/main/java/com/eldarovich99/remote_assistant/presentation/view/CallFragment.kt
+++ app/src/main/java/com/eldarovich99/remote_assistant/presentation/view/CallFragment.kt
@@ -5,13 +5,35 @@ import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.eldarovich99.remote_assistant.R
 import com.eldarovich99.remote_assistant.presentation.BaseFragment
+import com.eldarovich99.remote_assistant.presentation.ui.CloseConfirmationDialog
 import kotlinx.android.synthetic.main.fragment_call.*
 
 class CallFragment : BaseFragment(){
     override fun dispatchKeyEvent(event: KeyEvent?) {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (event?.keyCode){
+            KeyEvent.KEYCODE_DPAD_CENTER -> {
+            }
+            KeyEvent.KEYCODE_F1 -> {
+                Toast.makeText(context, "KEYCODE_F1", Toast.LENGTH_SHORT).show()}
+            KeyEvent.KEYCODE_F2 -> {
+                Toast.makeText(context, "KEYCODE_F2", Toast.LENGTH_SHORT).show()}
+            KeyEvent.KEYCODE_F3 -> {
+                Toast.makeText(context, "KEYCODE_F3", Toast.LENGTH_SHORT).show()}
+            KeyEvent.KEYCODE_F4 -> {
+                Toast.makeText(context, "KEYCODE_F4", Toast.LENGTH_SHORT).show()}
+            KeyEvent.KEYCODE_DPAD_LEFT -> {
+                CloseConfirmationDialog(this).get().show()
+            }
+            KeyEvent.KEYCODE_DPAD_RIGHT -> {
+            }
+            KeyEvent.KEYCODE_DPAD_DOWN -> {
+            }
+            KeyEvent.KEYCODE_DPAD_UP -> {
+            }
+        }
     }
 
     override fun onCreateView(
