580badc7ef88e771f21f539ec251032601ed2d54
eldarovich99 <32201422+eldarovich99@users.noreply.github.com>
2020-02-24 23:07:27 +0300

    added ViewHolders for items with pdf

--- app/src/main/java/com/eldarovich99/remote_assistant/domain/models/Message.kt
+++ app/src/main/java/com/eldarovich99/remote_assistant/domain/models/Message.kt
@@ -4,5 +4,7 @@ class Message (val type : Int){
     companion object{
         const val OWNER = 1
         const val COMPANION = 2
+        const val OWNER_FILE = 3
+        const val COMPANION_FILE = 4
     }
 }
--- app/src/main/java/com/eldarovich99/remote_assistant/presentation/view/call/SingleChatsAdapter.kt
+++ app/src/main/java/com/eldarovich99/remote_assistant/presentation/view/call/SingleChatsAdapter.kt
@@ -3,27 +3,36 @@ package com.eldarovich99.remote_assistant.presentation.view.call
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.eldarovich99.remote_assistant.R
 import com.eldarovich99.remote_assistant.domain.models.Message
 import javax.inject.Inject
 
 
-class SingleChatAdapter @Inject constructor(): RecyclerView.Adapter<SingleChatAdapter.SingleViewHolder>() {
+class SingleChatAdapter @Inject constructor(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     val items = generateSequence { Message(1) }.take(2).toList()
+        .plus(generateSequence { Message(3) }.take(2).toList())
+        .plus(generateSequence { Message(4) }.take(2).toList())
         .plus(generateSequence { Message(2) }.take(2).toList())
         .plus(generateSequence { Message(1) }.take(1).toList())
         .plus(generateSequence { Message(2) }.take(1).toList())
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleViewHolder {
-        val view : View = when (viewType){
-            Message.OWNER -> LayoutInflater.from(parent.context).inflate(R.layout.owner_message, parent, false)
-            Message.COMPANION -> LayoutInflater.from(parent.context).inflate(R.layout.companion_message, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType){
+            Message.OWNER ->{
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.owner_message, parent, false)
+                SingleViewHolder(view)
+            }
+            Message.COMPANION -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.companion_message, parent, false)
+                SingleViewHolder(view)
+            }
+            Message.OWNER_FILE -> FileViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.owner_message_pdf, parent, false))
+            Message.COMPANION_FILE -> FileViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.companion_message_pdf, parent, false))
             else -> throw Exception("Unknown message type")
         }
-        return SingleViewHolder(
-            view
-        )
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -34,11 +43,16 @@ class SingleChatAdapter @Inject constructor(): RecyclerView.Adapter<SingleChatAd
         return items.size
     }
 
-    override fun onBindViewHolder(holder: SingleViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
        // holder.itemNameTextView.text = "${holder.itemNameTextView.text.toString().substringBefore(' ')} ${holder.adapterPosition}"
     }
 
     class SingleViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        //val itemNameTextView = view.findViewById<TextView>(R.id.nameTextView)
+        val itemNameTextView: TextView = view.findViewById(R.id.messageTextView)
+    }
+
+    class FileViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val fileNameTextView: TextView = view.findViewById(R.id.fileTitleTextView)
+        val fileSizeTextView: TextView = view.findViewById(R.id.fileSizeTextView)
     }
 }
