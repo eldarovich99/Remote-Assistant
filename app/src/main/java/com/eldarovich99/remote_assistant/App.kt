88d64c27684ed985407d4e18c6826587a5e81ede
eldarovich99 <32201422+eldarovich99@users.noreply.github.com>
2020-03-08 15:55:02 +0300

    Dependency Injection runtime checks for debug build added

--- app/src/main/java/com/eldarovich99/remote_assistant/App.kt
+++ app/src/main/java/com/eldarovich99/remote_assistant/App.kt
@@ -4,12 +4,14 @@ import android.app.Application
 import com.eldarovich99.remote_assistant.di.Scopes
 import com.eldarovich99.remote_assistant.di.modules.AppModule
 import toothpick.Toothpick
+import toothpick.configuration.Configuration
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         val appScope = Toothpick.openScope(Scopes.APP_SCOPE)
         appScope.installModules(AppModule(this))
+        Toothpick.setConfiguration(if (BuildConfig.DEBUG) Configuration.forDevelopment() else Configuration.forProduction())
         //appScope.installModules(RepositoryModule(this))
     }
 }
